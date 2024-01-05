8c2d26dac9973f0ed95b4b6bcd6cbb5ae6d388db
Haf <haf@protonmail.ch>
2024-01-05 06:46:20 +0100

    added the symlink funs

--- ch2.el
+++ ch2.el
@@ -54,3 +54,62 @@
  )
 
 (global-set-key "\M-!" 'line-to-top)
+
+;; symbolic link stuff
+(defun read-only-if-symlink ()
+  (if (file-symlink-p buffer-file-name)
+      (progn
+	(setq buffer-read-only t)
+	(message "File is a symlink")
+      )
+  )
+)
+
+(add-hook 'find-file-hook 'read-only-if-symlink)
+
+;; lambda version
+(add-hook 'find-file-hooks
+	  '(lambda ()
+	     (if (file-symlink-p buffer-file-name)
+		 (progn
+		   (setq buffer-read-only t)
+		   (message "File is a symlink")
+		 )
+	      )
+	   )
+)
+
+(defun visit-target-instead ()
+  "Replace this buffer with a buffer visiting the link target"
+  (interactive)
+  (if buffer-file-name
+      (let
+	  ((target (file-symlink-p buffer-file-name)))
+	(if target
+	    (find-alternate-file target)
+	    (error "Not visiting a symlink")
+	 )
+       )
+       (error "Not visiting a file")
+   )
+ )
+
+(defun clobber-symlink ()
+  "Replace symlink with a copy of the file."
+  (interactive)
+  (if buffer-file-name
+      (let
+	  ((target (file-symlink-p buffer-file-name)))
+	(if target
+	    (if (yes-or-no-p (format "Replace %s with %s" buffer-file-name target))
+		(progn
+		  (delete-file buffer-file-name)
+		  (write-file buffer-file-name)
+		)
+	     )
+	  (error "Not visiting a symlink")
+	)
+      )
+      (error "Not visiting a file")
+   )
+)
