12d490173c223519f5de6c3b22f15822df869f35
Haf <haf@protonmail.ch>
2024-01-10 06:37:52 +0100

    added hscroll and window-start

--- ch3.el
+++ ch3.el
@@ -1,13 +1,24 @@
 (defvar unscroll-to nil
   "Text position for the next call to 'unscroll'.")
 
+(defvar unscroll-window-start nil
+  "Window start for next call to 'unscroll'.")
+
+(defvar unscroll-hscroll nil
+  "Hscroll for next call to 'unscroll'.")
+
 (defadvice scroll-up (before remember-for-unscrioll
 			     activate compile)
   "Remember where we started from, for 'unscroll'."
   (if (not (eq last-command 'scroll-up))
-      (setq unscroll-to (point))))
+      (progn
+	(setq unscroll-to (point))
+	(setq unscroll-window-start (window-start))
+	(setq unscroll-hscroll (window-hscroll)))))
 
 (defun unscroll ()
   "Jump to location specified by 'unscroll-to'."
   (interactive)
-  (goto-char unscroll-to))
+  (goto-char unscroll-to)
+  (set-window-start nil unscroll-window-start)
+  (set-window-hscroll nil unscroll-hscroll))
