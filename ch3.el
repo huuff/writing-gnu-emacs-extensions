567608fe2c548742cdd358b88644d7eee0a4c5da
Haf <haf@protonmail.ch>
2024-01-15 07:17:06 +0100

    finished the simplest version

--- ch3.el
+++ ch3.el
@@ -8,81 +8,32 @@
   "Hscroll for next call to 'unscroll'.")
 
 ;; TODO: Use modern advices instead
-(defadvice scroll-up (before remember-for-unscroll
-			     activate compile)
-  "Remember where we started from, for 'unscroll'."
-  (if (not (or (eq last-command 'scroll-up)
-	       (eq last-command 'scroll-down)
-	       (eq last-command 'scroll-left)
-	       (eq last-command 'scroll-right)))
-      (progn
-	(setq unscroll-point (point))
-	(setq unscroll-window-start (window-start))
-	(setq unscroll-hscroll (window-hscroll)))))
-
-(defadvice scroll-down (before remember-for-unscroll
-			       activate compile)
-  "Remember where we started from, for 'unscroll'."
-  (if (not (or (eq last-command 'scroll-down)
-	       (eq last-command 'scroll-up)
-	       (eq last-command 'scroll-left)
-	       (eq last-command 'scroll-right)))
-	   (setq unscroll-point (point)
-		 unscroll-window-start (window-start)
-		 unscroll-hscroll (window-hscroll))))
-
-(defadvice scroll-left (before remember-for-unscroll
-			       activate compile)
-  "Remember where we started from, for 'unscroll'"
-  (if (not (or (eq last-command 'scroll-up)
-	       (eq last-command 'scroll-down) t
-	       (eq last-command 'scroll-left)
-	       (eq last-command 'scroll-right)
-	       (setq unscroll-point (point)
-		     unscroll-windowstart (window-start)
-		     unscroll-hscroll (window-hscroll))))))
-
-(defadvice scroll-right (before remember-for-unscroll
-				activate compile)
-  "Remember where we started from, for 'unscroll'"
-  (if (not (or (eq last-command 'scroll-up)
-	       (eq last-command 'scroll-down)
-	       (eq last-command 'scroll-left)
-	       (eq last-command 'scroll-right)))))
 
 (defun unscroll ()
   "Jump to location specified by 'unscroll-point'."
   (interactive)
-  (if not unscroll-point
+  (if (not unscroll-point)
     (error "Cannot unscroll yet")
     (progn
 	(goto-char unscroll-point)
 	(set-window-start nil unscroll-window-start)
 	(set-window-hscroll nil unscroll-hscroll))))
 
-;; just eval this if I want a clean slate (TODO: Does it work? check it)
-(progn
-  (advice-remove 'scroll-up 'remember-for-unscroll)
-  (advice-remove 'scroll-down 'remember-for-unscroll)
-  (advice-remove 'scroll-left 'remember-for-unscroll)
-  (advice-remove 'scroll-right 'remember-for-unscroll)) 
 
-;; maybe remember version
 (defun unscroll-maybe-remember ()
-  (if (not (or (eq last-command 'scroll-up)
-	       (eq last-command 'scroll-down)
-	       (eq last-command 'scroll-left)
-	       (eq last-command 'scroll-right)))
-      (setq unscroll-point (point)
-	    unscroll-window-start (window-start)
-	    unscroll-hscroll (window-hscroll))))
+  (setq this-command 'unscrollable)
+  (if (not (eq last-command 'unscrollable))
+  (setq unscroll-point (point)
+	unscroll-window-start (window-start)
+	unscroll-hscrolll (window-hscroll))))
 
 (defadvice scroll-up (before remember-for-unscroll
 			     activate compile)
   "Remember where we started from, for 'unscroll'."
   (unscroll-maybe-remember))
 
-(defadvice scroll-down (before remember-for-unscroll)
+(defadvice scroll-down (before remember-for-unscroll
+			       activate compile)
   "Remember where we started from, for 'unscroll'."
   (unscroll-maybe-remember))
 
@@ -91,6 +42,49 @@
   "Remember where we started from, for 'unscroll'."
   (unscroll-maybe-remember))
 
-(defadvice scroll-right (before remember-for-unscroll)
+(defadvice scroll-right (before remember-for-unscroll
+				activate compile)
   "Remember where we started from, for 'unscroll'."
   (unscroll-maybe-remember))
+
+;; APPENDING SOME TEXT TO TEST IT
+asdasd
+
+sadsa
+dasdas
+dsad
+
+sdasdasdad
+sdasdasdasdsa
+
+
+asdasdasdasdasd
+asdasdsa
+ass
+dsadasdasdasdasd
+adasdasdsad
+
+
+sdadasdasdadasdas
+s
+asdasdasd
+asdasdasdsdad
+
+
+asdasdasdasdsadsd
+
+asdasdasdasdadasda
+asdsa
+asdads
+dd
+asdas
+dsa
+das
+das
+dasdsad
+asdasd
+asd
+asd
+asdasda
+dasdas
+da
